27b630c025c4a07a83cea283a09b6b88294d84d5
Yize Chen <yizechen@uw.edu>
2024-01-29 21:10:03 -0500

    Add files via upload

---
+++ Wind_Power_Forecasting/install.sh
@@ -0,0 +1,19 @@
+# conda create -n llmtime python=3.9
+# conda activate llmtime
+pip install numpy
+pip install -U jax[cpu] # we don't need GPU for jax
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+pip install openai 
+pip install tiktoken
+pip install tqdm
+pip install matplotlib
+pip install "pandas<2.0.0"
+pip install darts
+pip install gpytorch
+pip install transformers
+pip install datasets
+pip install multiprocess
+pip install SentencePiece
+pip install accelerate
+pip install gdown
+# conda deactivate llmtime
